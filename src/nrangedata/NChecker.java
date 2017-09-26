d78463cd14ab55f1cfde199ff2c2317d636cee10
Mayazure <mayazureee@gmail.com>
2017-09-26 23:39:56 +1000

    Create data checker.

---
+++ src/nrangedata/NChecker.java
@@ -0,0 +1,56 @@
+package nrangedata;
+
+import java.io.File;
+
+import nfileoperator.NDirTool;
+import nfileoperator.NFileOperator;
+
+public class NChecker {
+
+	private String path;
+	private NFileOperator fileOperator;
+	
+	public NChecker(String path){
+		this.path = path;
+		fileOperator = new NFileOperator();
+	}
+	
+	public void check(){
+		
+		File files[] = NDirTool.getFiles(path);
+		
+		for(File tempfile : files){
+			String tempfilePath = tempfile.getAbsolutePath();
+			fileOperator.loadReadFile(tempfilePath);
+			
+			int lastLine = 0;
+			int fileLine = 0;
+			String line;
+			while((line = fileOperator.nextLine())!=null){
+				fileLine ++;
+				line = line.trim();
+				if(line.equals("")){
+					continue;
+				}
+				String isTest = line.substring(line.length()-1);
+				if(isTest.equals("1")){
+					System.out.println("In <"+tempfilePath+", line "+fileLine + ">: " + "is test.");
+				}
+				
+				String lineNo = line.substring(0, line.indexOf(","));
+				int currentLine = Integer.valueOf(lineNo);
+				if(lastLine<currentLine){
+					lastLine = currentLine;
+				}
+				else{
+					System.out.println("In <"+tempfilePath+", line "+fileLine + ">: " + currentLine);
+				}
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		NChecker checker = new NChecker("H:\\Myo\\Data\\Processing\\modified\\");
+		checker.check();
+	}
+}
--- src/test/GeneralTest.java
+++ src/test/GeneralTest.java
@@ -5,9 +5,21 @@ import nconfig.NConfig;
 public class GeneralTest {
 
 	public static void main(String[] args) {
-		
+		//test1();
+		test2();
+	}
+	
+	private static void test1(){
 		for(int i = 0;i<NConfig.level.length;i++){
 			System.out.println(i+": "+NConfig.level[i]);
 		}
+	}
+	
+	private static void test2() {
+		String in = "441879,2017-07-18 12:35:35,1500345330558,1500345335997,82,29,0";
+		String out = in.substring(0,in.indexOf(","));
+		String last = in.substring(in.length()-1);
+		System.out.println(out);
+		System.out.println(last);
 	}
 }
