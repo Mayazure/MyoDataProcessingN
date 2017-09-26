9ee974a8ca964360221b80f6e1418a3ef50f44eb
Mayazure <mayazureee@gmail.com>
2017-09-26 23:11:30 +1000

    Update.

--- src/nconfig/NConfig.java
+++ src/nconfig/NConfig.java
@@ -4,7 +4,7 @@ public class NConfig {
 
 	public static final int[] level = {
 			0,0,0,0,0,0,0,0,0,0,0,
-			0,0,0,0,0,0,0,0,0,1,
+			0,0,0,0,0,0,0,1,1,1,
 			1,1,1,1,1,1,1,1,1,1,
 			1,1,1,1,1,1,1,1,1,1,
 			1,1,1,1,1,2,2,2,2,2,
@@ -18,5 +18,5 @@ public class NConfig {
 	public static final int distribution = 3;
 	public static final int windowLength = 300;
 	public static final int totalExp = 29;
-	public static final int startFrom = 0;
+	public static final int startFrom = 2;
 }
